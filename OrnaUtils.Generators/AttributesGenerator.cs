91b2508f54e175927708ac54e053a58af4696010
ornaras <ornaras.us@gmail.com>
2025-07-02 17:20:39 +0400

    Реализован генератор атрибутов

---
+++ OrnaUtils.Generators/AttributesGenerator.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace OrnaUtils.Generators
+{
+    [Generator]
+    public class AttributesGenerator : IIncrementalGenerator
+    {
+        public void Initialize(IncrementalGeneratorInitializationContext context)
+        {
+            context.RegisterPostInitializationOutput(ctx => 
+                ctx.AddSource("Attributes.g.cs", $$"""
+                    namespace OrnaLibs.Attributes
+                    {
+                        {{NotifyChangedAttribute}}
+                    }
+                    """));
+        }
+
+        private string NotifyChangedAttribute => """
+            [AttributeUsage(AttributeTargets.Field)]
+                public class NotifyChangedAttribute : Attribute
+                {
+                    public NotifyChangedAttribute(params string[] otherProps) { }
+                }
+
+            """;
+    }
+}
